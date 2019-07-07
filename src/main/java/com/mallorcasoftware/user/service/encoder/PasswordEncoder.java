4487dadb759a3c2bb2433f4d842aa3f7ecd5bb0c
Pascal Wild <pascal.wild@mallorca-software.com>
2019-07-07 13:50:50 +0200

    Add matches method to PasswordEncoder

--- src/main/java/com/mallorcasoftware/user/service/encoder/PasswordEncoder.java
+++ src/main/java/com/mallorcasoftware/user/service/encoder/PasswordEncoder.java
@@ -2,4 +2,5 @@ package com.mallorcasoftware.user.service.encoder;
 
 public interface PasswordEncoder {
     String encode(CharSequence value);
+    boolean matches(CharSequence rawPassword, String encodedPassword);
 }
